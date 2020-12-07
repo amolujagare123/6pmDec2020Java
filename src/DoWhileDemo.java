43fcca964c50a944655d8d40708991b865945c29
PC <amolujagare@gmail.com>
2020-12-07 19:14:03 +0530

    till loops

---
+++ src/DoWhileDemo.java
@@ -0,0 +1,18 @@
+public class DoWhileDemo {
+
+    public static void main(String[] args) {
+
+        int i=1; // initialization
+
+        do {
+
+            System.out.println(i);
+            i = i+1; // increment / decrement
+
+        } while(i<=10); // condition checking
+
+
+
+    }
+
+}
---
+++ src/ForLoopDemo2.java
@@ -0,0 +1,12 @@
+public class ForLoopDemo2 {
+
+    public static void main(String[] args) {
+
+
+        for(int i=10; i>=1 ; i--)
+            System.out.print(i+"\t");
+
+        System.out.println("My name is amol");
+
+    }
+}
